455d287ebc742c1c16dc6389f42ec5334af482e1
Shawn Daichendt <shawn.daichendt@gmail.com>
2016-08-30 15:57:38 -0400

    created Test model

--- server.js
+++ server.js
@@ -2,11 +2,14 @@
 // =============================================================================
 
 // call the packages/ data we need
-var express    = require('express');
-var bodyParser = require('body-parser');
-var mongoose   = require('mongoose');
-var mongodbUri = require('mongodb-uri');
-var db         = require('./db.json');
+var express     = require('express');
+var bodyParser  = require('body-parser');
+var mongoose    = require('mongoose');
+var mongodbUri  = require('mongodb-uri');
+var db          = require('./db.json');
+
+// setup models
+var Test        = require('./app/models/test');
 
 // connect to our database
 var uri = mongodbUri.format(db);
@@ -28,10 +31,19 @@ var port = process.env.PORT || 8090;
 // get an instance of the express Router
 var router = express.Router();
 
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+
+    // make sure we go to the next routes and don't stop here
+    next();
+});
+
 // test route to make sure everything is working
 // (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-  res.json({ message: 'hooray! welcome to our api!' });
+  res.json({ message: 'Hooray! Welcome to our API!' });
 });
 
 // more routes for our API will happen here
@@ -43,5 +55,6 @@ app.use('/api', router);
 // START THE SERVER
 // =============================================================================
 app.listen(port);
+
 console.log('API Server running on port: ' + port);
 console.log('MongoDB running at: ' + uri);
