a0eaa084a2dcd008885e11e5b56cfd20f99ed926
Shawn Daichendt <shawn.daichendt@gmail.com>
2016-09-06 14:23:17 -0400

    updated scoreCard_spec with new tests

--- app/tests/scoreCard_spec.js
+++ app/tests/scoreCard_spec.js
@@ -7,12 +7,12 @@ require('sinon-mongoose');
 
 var ScoreCard = require('../models/scoreCard');
 
-describe("ScoreCard", function() {
+describe("Get all scoreCards", function() {
   it("should return all scoreCards", function(done) {
     var ScoreCardMock = sinon.mock(ScoreCard);
     var expectedResult = {status: true, scoreCard: []};
-
     ScoreCardMock.expects('find').yields(null, expectedResult);
+
     ScoreCard.find(function (err, result) {
       ScoreCardMock.verify();
       ScoreCardMock.restore();
@@ -21,12 +21,100 @@ describe("ScoreCard", function() {
     });
   });
 
-  it("should return error", function(done) {
+  it("should return error if the action failed", function(done) {
     var ScoreCardMock = sinon.mock(ScoreCard);
     var expectedResult = {status: false, error: "Something went wrong"};
-
     ScoreCardMock.expects('find').yields(expectedResult, null);
+
     ScoreCard.find(function (err, result) {
+      ScoreCardMock.verify();
+      ScoreCardMock.restore();
+      expect(err.status).to.not.be.true;
+      done();
+    });
+  });
+});
+
+describe("Create a new scoreCard", function() {
+  it("should create new scoreCard", function(done) {
+    var ScoreCardMock = sinon.mock(new ScoreCard({ title: 'Title' }));
+    var scoreCard = ScoreCardMock.object;
+    var expectedResult = { status: true };
+    ScoreCardMock.expects('save').yields(null, expectedResult);
+
+    scoreCard.save(function (err, result) {
+      ScoreCardMock.verify();
+      ScoreCardMock.restore();
+      expect(result.status).to.be.true;
+      done();
+    });
+  });
+
+  it("should return error if the action failed", function(done) {
+    var ScoreCardMock = sinon.mock(new ScoreCard({ title: 'Title' }));
+    var scoreCard = ScoreCardMock.object;
+    var expectedResult = { status: false };
+    ScoreCardMock.expects('save').yields(expectedResult, null);
+
+    scoreCard.save(function (err, result) {
+      ScoreCardMock.verify();
+      ScoreCardMock.restore();
+      expect(err.status).to.not.be.true;
+      done();
+    });
+  });
+});
+
+describe("Update a scoreCard by id", function() {
+  it("should update a scoreCard by id", function(done) {
+    var ScoreCardMock = sinon.mock(new ScoreCard({ title: 'Title' }));
+    var scoreCard = ScoreCardMock.object;
+    var expectedResult = { status: true };
+    ScoreCardMock.expects('save').withArgs({_id: 12345}).yields(null, expectedResult);
+
+    scoreCard.save({_id: 12345}, function (err, result) {
+      ScoreCardMock.verify();
+      ScoreCardMock.restore();
+      expect(result.status).to.be.true;
+      done();
+    });
+  });
+
+  it("should return error if the action failed", function(done) {
+    var ScoreCardMock = sinon.mock(new ScoreCard({ title: 'Title' }));
+    var scoreCard = ScoreCardMock.object;
+    var expectedResult = { status: false };
+    ScoreCardMock.expects('save').withArgs({_id: 12345}).yields(expectedResult, null);
+
+    scoreCard.save({_id: 12345}, function (err, result) {
+      ScoreCardMock.verify();
+      ScoreCardMock.restore();
+      expect(err.status).to.not.be.true;
+      done();
+    });
+  });
+});
+
+describe("Delete a scoreCard by id", function(){
+  it("should delete a scoreCard by id", function(done){
+    var ScoreCardMock = sinon.mock(ScoreCard);
+    var expectedResult = { status: true };
+    ScoreCardMock.expects('remove').withArgs({_id: 12345}).yields(null, expectedResult);
+
+    ScoreCard.remove({_id: 12345}, function (err, result) {
+      ScoreCardMock.verify();
+      ScoreCardMock.restore();
+      expect(result.status).to.be.true;
+      done();
+    });
+  });
+
+  it("should return error if the action failed", function(done){
+    var ScoreCardMock = sinon.mock(ScoreCard);
+    var expectedResult = { status: false };
+    ScoreCardMock.expects('remove').withArgs({_id: 12345}).yields(expectedResult, null);
+
+    ScoreCard.remove({_id: 12345}, function (err, result) {
       ScoreCardMock.verify();
       ScoreCardMock.restore();
       expect(err.status).to.not.be.true;
