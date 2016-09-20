e72149d123c2adfc33a6d46ef6b7bea34c086c00
Shawn Daichendt <shawn.daichendt@gmail.com>
2016-09-20 16:36:11 -0400

    updated Page fixtures

--- app/fixtures/page.js
+++ app/fixtures/page.js
@@ -63,8 +63,12 @@ exports.Page = [{
     title: 'Target',
     value: '1,500'
   }, {
-    key: 'registered',
+    key: 'tab-4',
     title: 'Registered',
+    value: '0'
+  }, {
+    key: 'registered-caregivers',
+    title: 'Registered Caregivers',
     value: '0'
   }],
   scoreCards: [{
@@ -211,6 +215,42 @@ exports.Page = [{
   }, {
     key: 'neighbourhood-subgroup',
     title: 'Neighbourhoods',
+    list: []
+  }]
+},
+
+
+{
+  key: 'shared-outcomes-test-page',
+  title: 'Shared Outcomes',
+  infoBoxes: [{
+    key: 'registered-caregivers',
+    title: 'Registered Caregivers',
+    value: '0'
+  }],
+  scoreCards: [{
+    key: 'people-subgroup',
+    title: 'People',
+    list: [{
+      key: 'quality-of-life',
+      title: 'Quality of Life',
+      score: 'A'
+    }, {
+      key: 'experience',
+      title: 'Experience',
+      score: 'B+'
+    }, {
+      key: 'expected',
+      title: 'Expected',
+      score: 'C-'
+    }, {
+      key: 'burden',
+      title: 'Burden',
+      score: '87.2'
+    }]
+  }, {
+    key: 'core-partners-subgroup',
+    title: 'Core Partners',
     list: []
   }]
 }];
