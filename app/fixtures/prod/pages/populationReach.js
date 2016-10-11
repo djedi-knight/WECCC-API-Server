470ffca4f043b9bc27df231893cc06eb1f3bf9f1
Shawn Daichendt <shawn.daichendt@gmail.com>
2016-10-11 12:35:56 -0400

    updated populationReach fixtures

--- app/fixtures/prod/pages/populationReach.js
+++ app/fixtures/prod/pages/populationReach.js
@@ -28,7 +28,7 @@ exports.Page = [{
     value: '0'
   }],
   scoreCards: [{
-    key: 'priority-subgroup',
+    key: 'priority-subgroup-1',
     title: 'Priority Subgroup',
     list: [{
       key: 'over-80',
@@ -42,7 +42,7 @@ exports.Page = [{
       trend: 'DOWN'
     }]
   }, {
-    key: 'dependency-subgroup',
+    key: 'dependency-subgroup-1',
     title: 'Dependency Level',
     list: [{
       key: 'light',
@@ -66,7 +66,7 @@ exports.Page = [{
       trend: 'NONE'
     }]
   }, {
-    key: 'registered-subgroup',
+    key: 'registered-subgroup-1',
     title: 'Registed To',
     list: [{
       key: 'care',
@@ -83,6 +83,63 @@ exports.Page = [{
       title: 'Neighbourhood',
       score: '20',
       trend: 'DOWN'
+    }]
+  }, {
+    key: 'priority-subgroup-2',
+    title: 'Priority Subgroup',
+    list: [{
+      key: 'over-80',
+      title: '80+',
+      score: '68',
+      trend: 'DOWN'
+    }, {
+      key: 'disabled',
+      title: 'Disabled',
+      score: '20',
+      trend: 'UP'
+    }]
+  }, {
+    key: 'dependency-subgroup-2',
+    title: 'Dependency Level',
+    list: [{
+      key: 'light',
+      title: 'Light',
+      score: '99',
+      trend: 'NONE'
+    }, {
+      key: 'moderate',
+      title: 'Moderate',
+      score: '95',
+      trend: 'DOWN'
+    }, {
+      key: 'complex',
+      title: 'Complex',
+      score: '50',
+      trend: 'UP'
+    }, {
+      key: 'eol',
+      title: 'EOL',
+      score: '20',
+      trend: 'DOWN'
+    }]
+  }, {
+    key: 'registered-subgroup-2',
+    title: 'Registed To',
+    list: [{
+      key: 'care',
+      title: 'Care',
+      score: '100',
+      trend: 'NONE'
+    }, {
+      key: 'outreach',
+      title: 'Outreach',
+      score: '95',
+      trend: 'DOWN'
+    }, {
+      key: 'neighbourhood',
+      title: 'Neighbourhood',
+      score: '30',
+      trend: 'UP'
     }]
   }]
 }];
