e1ee9a0c8048960cd7421cd1d67098dfb4567e42
Shawn Daichendt <shawn.daichendt@gmail.com>
2016-10-19 17:18:58 -0400

    added fixtures for VitalSigns page

---
+++ app/fixtures/prod/pages/vitalSigns.js
@@ -0,0 +1,68 @@
+/* global exports  */
+/* global require  */
+
+require('../../../models/page');
+
+exports.Page = [{
+  key: 'vital-signs-page',
+  title: 'Vital Signs',
+  scoreCards: [{
+    key: 'vital-signs-subgroup',
+    title: 'Vital Signs',
+    list: [{
+      key: 'government',
+      title: 'Government',
+      score: 'B+',
+      trend: 'UP'
+    }, {
+      key: 'safety',
+      title: 'Safety',
+      score: 'D',
+      trend: 'UP'
+    }, {
+      key: 'arts-culture',
+      title: 'Arts & Culture',
+      score: 'B',
+      trend: 'DOWN'
+    }, {
+      key: 'transportation',
+      title: 'Transportation',
+      score: 'C-',
+      trend: 'NONE'
+    }, {
+      key: 'food-security',
+      title: 'Food Security',
+      score: 'B+',
+      trend: 'DOWN'
+    }]
+  }, {
+    key: 'self-reported-subgroup',
+    title: 'Self-Reported',
+    list: [{
+      key: 'government',
+      title: 'Government',
+      score: 'A+',
+      trend: 'UP'
+    }, {
+      key: 'safety',
+      title: 'Safety',
+      score: 'C',
+      trend: 'DOWN'
+    }, {
+      key: 'arts-culture',
+      title: 'Arts & Culture',
+      score: 'A',
+      trend: 'UP'
+    }, {
+      key: 'transportation',
+      title: 'Transportation',
+      score: 'D',
+      trend: 'DOWN'
+    }, {
+      key: 'food-security',
+      title: 'Food Security',
+      score: 'A+',
+      trend: 'NONE'
+    }]
+  }]
+}];
