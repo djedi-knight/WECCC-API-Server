1e3990ebac0082c6ee6e5996b70686be94bd0873
Shawn Daichendt <shawn.daichendt@gmail.com>
2016-11-15 16:02:15 -0500

    fixed parse_shared_outcomes.pl

--- app/scripts/parse_shared_outcomes.pl
+++ app/scripts/parse_shared_outcomes.pl
@@ -8,8 +8,7 @@ use Text::CSV;
 my $filename = "import_data.csv";
 my $output_dir = "output";
 my $output_filename = "/sharedOutcomes.js";
-my @rows;                       # input document rows
-my %pages;                      # output JSON documents organized by page
+my @rows; # input document rows
 
 my $csv = Text::CSV->new ({binary=>1})
     or die "Cannot use CSV: ".Text::CSV->error_diag();
@@ -36,50 +35,41 @@ my $firstRow = $csv->getline($fh);
 #  Each different element type will require different handling
 
 my @lastrow;
-my $output = "/* global exports  */\n".
-             "/* global require  */\n".
-             "require('../../../models/page');\n".
-             "exports.Page = ";
+my $output = "";
+
 while (my $row_r = $csv->getline($fh)){
 
     my @row = @{$row_r};
-    my $rowdata; # processed row data
     my $page = $row[0];
 
-    #print $page . "\n";
     next if ($page ne "shared-outcomes-page");
 
-    if ($output eq ""){ # new page data
-
-    $output = "[{".
-              "key: '$row[0]',".
-              "title: '$row[1]',";
-
+    if ($output eq "") { # new page data
+      $output = $output . processPageStart(\@row);
     }
-    if ($row[2] eq "infoboxes"){
-
+    if ($row[2] eq "infoboxes") {
         my $infoboxJSON = processInfobox(\@row,\@lastrow);
         $output = $output . $infoboxJSON;
     }
-    if ($row[2] eq "scoreCards"){
-
+    if ($row[2] eq "scoreCards") {
         my $scorecardJSON = processScoreCard(\@row,\@lastrow);
         $output = $output . $scorecardJSON;
     }
 
     @lastrow = @row; # previous row's data - sometimes needed for context
-
 }
 
-# Close off unclosed arrays
+# Close off the page
 
-$output = $output . "]\n}]\n}];";
+$output = $output . processPageEnd();
 
 # Close CSV input file
+
 $csv->eof or $csv->error_diag();
 close $fh;
 
 # Write output to file
+
 mkdir ($output_dir) unless (-d $output_dir);
 open (my $output_fh, '>', $output_dir . $output_filename);
 print $output_fh $output;
@@ -87,57 +77,90 @@ close $output_hf;
 
 # Functions
 
-sub processInfobox{
-    # Shared outcomes page has one infobox. Those with a list will require a different
-    # subroutine.
-    my $infobox_output;
+sub processPageStart {
+  my @row= @{$_[0]};
+  my $page_output = <<"END_OUTPUT";
+/* global exports  */
+/* global require  */
 
+"require('../../../models/page');"
+
+exports.Page = [{
+  key: '$row[0]',
+  title: '$row[1]',
+END_OUTPUT
+  return $page_output;
+}
+
+sub processPageEnd {
+  my $page_output = <<"END_OUTPUT";
+    }]
+  }]
+}];
+END_OUTPUT
+  return $page_output;
+}
+
+sub processInfobox {
+  my @row= @{$_[0]};
+  my @lastrow = @{$_[1]};
+  my $infobox_output;
+
+  if ($lastrow[2] ne "infoboxes") {  # add preamble for infobox if it's the first
+    $infobox_output = <<"END_OUTPUT";
+  infoBoxes: [{
+END_OUTPUT
+  } else { # close off infobox if it isn't
+    $infobox_output = $infobox_output . <<"END_OUTPUT";
+  }
+END_OUTPUT
+  }
+
+  $infobox_output = $infobox_output .  <<"END_OUTPUT";
+    key: '$row[5]',
+    title: '$row[6]',
+    value: '$row[7]'
+  }],
+END_OUTPUT
+
+  return $infobox_output;
+}
+
+sub processScoreCard {
     my @row= @{$_[0]};
-
     my @lastrow = @{$_[1]};
-
-    #print $row[0]."\n";
-
-if ($lastrow[2] ne "infoboxes"){
-    $infobox_output = "\ninfoboxes:[{"; # add preamble for infobox if it's the first
-}else{
-    $infobox_output = $infobox_output."},\n"; # close off infobox if it isn't
-}
- $infobox_output = $infobox_output."key: '".$row[5]."',\n".
-                                  "title: '".$row[6]."',\n".
-                                  "value: '".$row[7]."'\n".
-                                  "}]";
-
-return $infobox_output;
-}
-
-sub processScoreCard{
     my $scorecard_output;
 
-    my @row = @{$_[0]};
-
-    my @lastrow = @{$_[1]};
     if ($lastrow[2] ne "scoreCards"){ # add preamble for scorecards if it's the first
-        $scorecard_output = $scorecard_output.",\nscoreCards:[\n";
-    }else{
-        if ($lastrow[3] eq $row[3]){ # add comma unless we are closing off a list
-            $scorecard_output = $scorecard_output.",";
-        }
+      $scorecard_output = <<"END_OUTPUT";
+  scoreCards: [{
+END_OUTPUT
+    } else {
+      if ($lastrow[3] eq $row[3]) { # add comma unless we are closing off a list
+        $scorecard_output = $scorecard_output . <<"END_OUTPUT";
+    }, {
+END_OUTPUT
+      }
     }
 
-    if ($lastrow[3] ne $row[3]){ # add preamble to new infoboxes
-                if ($lastrow[3] ne ""){ # close off previous list if not first set infobox
-                    $scorecard_output = $scorecard_output. "]\n},";
-                }
-            $scorecard_output = $scorecard_output .
-            "{\nkey:'".$row[3]."',\n".
-            "title:'".$row[4]."',\n".
-            "list:[";
+    if ($lastrow[3] ne $row[3]){
+      if ($lastrow[3] ne ""){ # close off previous list if not first set scorebox
+        $scorecard_output = $scorecard_output . <<"END_OUTPUT";
+    }]
+  }, {
+END_OUTPUT
+      }
 
+      $scorecard_output = $scorecard_output . <<"END_OUTPUT";
+    key: '$row[3]',
+    title: '$row[4]',
+    list: [{
+END_OUTPUT
     }
-    $scorecard_output = $scorecard_output."{key: '".$row[5]."',\n".
-                                           "title:'".$row[6]."',\n".
-                                           "score:'".$row[7]."'}\n";
 
-
+    $scorecard_output = $scorecard_output . <<"END_OUTPUT";
+      key: '$row[5]',
+      title: '$row[6]',
+      score: '$row[7]'
+END_OUTPUT
 }
