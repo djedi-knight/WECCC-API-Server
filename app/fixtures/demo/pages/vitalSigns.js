0937ab7c668f6e39db581813573ce2a5ad337aae
Shawn Daichendt <shawn.daichendt@gmail.com>
2016-11-22 13:10:45 -0500

    updated vitalSigns data

--- app/fixtures/demo/pages/vitalSigns.js
+++ app/fixtures/demo/pages/vitalSigns.js
@@ -7,13 +7,33 @@ exports.Page = [{
   key: 'vital-signs-demo-page',
   title: 'Vital Signs',
   scoreCards: [{
-    key: 'vital-signs-subgroup',
-    title: 'Vital Signs',
+    key: 'windsor-subgroup',
+    title: 'Windsor',
     list: [{
-      key: 'government',
-      title: 'Government',
+      key: 'belonging-leadership',
+      title: 'Belonging and Leadership',
       score: 'B+',
       trend: 'UP'
+    }, {
+      key: 'health',
+      title: 'Health',
+      score: 'A',
+      trend: 'UP'
+    }, {
+      key: 'wellness-activity',
+      title: 'Wellness and Activity',
+      score: 'C',
+      trend: 'DOWN'
+    }, {
+      key: 'work',
+      title: 'Work',
+      score: 'B+',
+      trend: 'NONE'
+    }, {
+      key: 'housing',
+      title: 'Housing',
+      score: 'C-',
+      trend: 'DOWN'
     }, {
       key: 'safety',
       title: 'Safety',
@@ -23,46 +43,224 @@ exports.Page = [{
       key: 'arts-culture',
       title: 'Arts & Culture',
       score: 'B',
-      trend: 'DOWN'
+      trend: 'UP'
     }, {
-      key: 'transportation',
-      title: 'Transportation',
+      key: 'environment',
+      title: 'Environment',
       score: 'C-',
       trend: 'NONE'
+    }, {
+      key: 'learning',
+      title: 'Learning',
+      score: 'B+',
+      trend: 'DOWN'
+    }, {
+      key: 'getting-started',
+      title: 'Getting Started',
+      score: 'A-',
+      trend: 'UP'
     }, {
       key: 'food-security',
       title: 'Food Security',
       score: 'B+',
       trend: 'DOWN'
+    }, {
+      key: 'getting-around',
+      title: 'Getting Around',
+      score: 'C+',
+      trend: 'NONE'
     }]
   }, {
-    key: 'self-reported-subgroup',
-    title: 'Self-Reported',
+    key: 'essex-subgroup',
+    title: 'Essex',
     list: [{
-      key: 'government',
-      title: 'Government',
+      key: 'belonging-leadership',
+      title: 'Belonging and Leadership',
       score: 'A+',
-      trend: 'UP'
+      trend: 'NONE'
+    }, {
+      key: 'health',
+      title: 'Health',
+      score: 'B-',
+      trend: 'DOWN'
+    }, {
+      key: 'wellness-activity',
+      title: 'Wellness and Activity',
+      score: 'A',
+      trend: 'DOWN'
+    }, {
+      key: 'work',
+      title: 'Work',
+      score: 'D+',
+      trend: 'DOWN'
+    }, {
+      key: 'housing',
+      title: 'Housing',
+      score: 'C-',
+      trend: 'NONE'
     }, {
       key: 'safety',
       title: 'Safety',
       score: 'C',
-      trend: 'DOWN'
+      trend: 'NONE'
     }, {
       key: 'arts-culture',
       title: 'Arts & Culture',
+      score: 'B+',
+      trend: 'UP'
+    }, {
+      key: 'environment',
+      title: 'Environment',
+      score: 'D-',
+      trend: 'DOWN'
+    }, {
+      key: 'learning',
+      title: 'Learning',
       score: 'A',
       trend: 'UP'
     }, {
-      key: 'transportation',
-      title: 'Transportation',
-      score: 'D',
+      key: 'getting-started',
+      title: 'Getting Started',
+      score: 'B',
       trend: 'DOWN'
     }, {
       key: 'food-security',
       title: 'Food Security',
-      score: 'A+',
+      score: 'C+',
+      trend: 'DOWN'
+    }, {
+      key: 'getting-around',
+      title: 'Getting Around',
+      score: 'C',
       trend: 'NONE'
+    }]
+  }, {
+    key: 'seniors-subgroup',
+    title: 'Seniors',
+    list: [{
+      key: 'belonging-leadership',
+      title: 'Belonging and Leadership',
+      score: 'A+',
+      trend: 'UP'
+    }, {
+      key: 'health',
+      title: 'Health',
+      score: 'B',
+      trend: 'UP'
+    }, {
+      key: 'wellness-activity',
+      title: 'Wellness and Activity',
+      score: 'D',
+      trend: 'DOWN'
+    }, {
+      key: 'work',
+      title: 'Work',
+      score: 'A+',
+      trend: 'UP'
+    }, {
+      key: 'housing',
+      title: 'Housing',
+      score: 'B+',
+      trend: 'UP'
+    }, {
+      key: 'safety',
+      title: 'Safety',
+      score: 'B+',
+      trend: 'NONE'
+    }, {
+      key: 'arts-culture',
+      title: 'Arts & Culture',
+      score: 'A',
+      trend: 'NONE'
+    }, {
+      key: 'environment',
+      title: 'Environment',
+      score: 'C+',
+      trend: 'UP'
+    }, {
+      key: 'learning',
+      title: 'Learning',
+      score: 'A-',
+      trend: 'DOWN'
+    }, {
+      key: 'getting-started',
+      title: 'Getting Started',
+      score: 'A+',
+      trend: 'UP'
+    }, {
+      key: 'food-security',
+      title: 'Food Security',
+      score: 'B-',
+      trend: 'NONE'
+    }, {
+      key: 'getting-around',
+      title: 'Getting Around',
+      score: 'F',
+      trend: 'DOWN'
+    }]
+  }, {
+    key: 'youth-subgroup',
+    title: 'Youth',
+    list: [{
+      key: 'belonging-leadership',
+      title: 'Belonging and Leadership',
+      score: 'C-',
+      trend: 'DOWN'
+    }, {
+      key: 'health',
+      title: 'Health',
+      score: 'B-',
+      trend: 'DOWN'
+    }, {
+      key: 'wellness-activity',
+      title: 'Wellness and Activity',
+      score: 'C+',
+      trend: 'NONE'
+    }, {
+      key: 'work',
+      title: 'Work',
+      score: 'B-',
+      trend: 'DOWN'
+    }, {
+      key: 'housing',
+      title: 'Housing',
+      score: 'F',
+      trend: 'DOWN'
+    }, {
+      key: 'safety',
+      title: 'Safety',
+      score: 'B+',
+      trend: 'UP'
+    }, {
+      key: 'arts-culture',
+      title: 'Arts & Culture',
+      score: 'B-',
+      trend: 'DOWN'
+    }, {
+      key: 'environment',
+      title: 'Environment',
+      score: 'A+',
+      trend: 'UP'
+    }, {
+      key: 'learning',
+      title: 'Learning',
+      score: 'B',
+      trend: 'DOWN'
+    }, {
+      key: 'getting-started',
+      title: 'Getting Started',
+      score: 'B+',
+      trend: 'DOWN'
+    }, {
+      key: 'food-security',
+      title: 'Food Security',
+      score: 'C-',
+      trend: 'DOWN'
+    }, {
+      key: 'getting-around',
+      title: 'Getting Around',
+      score: 'B+',
+      trend: 'UP'
     }]
   }]
 }];
