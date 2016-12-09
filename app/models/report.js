51d688722cf85be3e75bd54bf7b163a7394ede8b
Shawn Daichendt <shawn.daichendt@gmail.com>
2016-12-09 16:20:05 -0500

    Revert "updated report model/ progressTracker fixtures"
    This reverts commit 48575ce48d79aa5497f14f350072d16e40b4bcc9.

--- app/models/report.js
+++ app/models/report.js
@@ -7,15 +7,12 @@ var ProgressReportSchema = new mongoose.Schema({
   key: { type: String, required: true },
   title: { type: String, required: true },
   data: [{
-    header: { type: String },
-    rows: [{
-      indicator: { type: String },
-      goal: { type: String },
-      baseline: { type: String },
-      change: { type: String },
-      trend: { type: String, enum: ['UP', 'DOWN', 'NONE'] },
-      colourCode: { type: String, enum: ['GREEN', 'YELLOW', 'RED'] }
-    }]
+    indicator: { type: String },
+    goal: { type: String },
+    baseline: { type: String },
+    change: { type: String },
+    trend: { type: String, enum: ['UP', 'DOWN', 'NONE'] },
+    colourCode: { type: String, enum: ['GREEN', 'YELLOW', 'RED'] }
   }]
 });
 
